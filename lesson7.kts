b37a235c666d8bdc7f7054fe8c8d65e64e681975
Ismoil-cmd <84372233+Ismoil-cmd@users.noreply.github.com>
2022-09-20 16:31:25 +0300

    Add files via upload

---
+++ lesson7.kts
@@ -0,0 +1,14 @@
+import java.lang.Math.pow
+import java .util.*
+
+var scan = Scanner(System.`in`);
+
+var d = scan.nextInt()
+var p = 3.14;
+println(d*p)
+
+var a = scan.nextDouble()
+
+println(pow(a,3.0))
+
+println(6* pow(a,2.0))
