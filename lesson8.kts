1ab280ba2b6163c008efed6e9d411000424eea71
Ismoil-cmd <84372233+Ismoil-cmd@users.noreply.github.com>
2022-09-20 16:41:10 +0300

    Add files via upload

---
+++ lesson8.kts
@@ -0,0 +1,18 @@
+import java.util.*
+
+var scan = Scanner(System.`in`);
+
+println("Integer 7")
+
+var n = scan.nextInt()
+var a = n% 10
+var b = n /10
+
+println(a + b)
+
+
+println("Integer 10")
+
+n = scan.nextInt()
+print(n%10)
+print(n/10%10)
