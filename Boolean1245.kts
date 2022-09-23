66088077c0faac50c7ddb7cc7f38af08fde37bb9
Ismoil-cmd <84372233+Ismoil-cmd@users.noreply.github.com>
2022-09-23 16:16:04 +0300

    Boolean 1,2,4,5

---
+++ Boolean1245.kts
@@ -0,0 +1,29 @@
+import java.util.*
+
+var scan = Scanner(System.`in`)
+
+println("Boolean1")
+
+var a = scan.nextInt()
+println(a > 0)
+
+println("Boolean2")
+
+a = scan.nextInt()
+
+println(a%2 !=0)
+
+println("Boolean4")
+
+a = scan.nextInt()
+var b = scan.nextInt()
+
+println(a > 2 && b <= 3)
+
+println("Boolean5")
+
+a = scan.nextInt()
+b = scan.nextInt()
+
+println(a >= 0 && b <-2)
+
